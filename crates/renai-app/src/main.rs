c12d8a8a34742a9e80c5e8df769a5f236cfe98ad
Vostanis <kimonvostanis@gmail.com>
2024-08-21 17:06:46 +0100

    working rocket framework - using html handlebars

---
+++ crates/renai-app/src/main.rs
@@ -0,0 +1,20 @@
+use rocket::{get, launch, routes};
+use rocket::fs::{FileServer, Options, relative};
+use rocket_dyn_templates::{context, Template};
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        // add templating system
+        .attach(Template::fairing())
+        // serve content from disk
+        .mount("/public", FileServer::new(relative!("/public"), Options::Missing | Options::NormalizeDirs))
+        // register routes
+        .mount("/", routes![root])
+}
+
+#[get("/")]
+async fn root() -> Template {
+    Template::render("root", context! { message: "renai"})
+}
+
